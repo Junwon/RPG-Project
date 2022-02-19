e1de77bd1d52da4b3c483860567595e7b5eacb7a
Junwon <junwon.kaihatsu@gmail.com>
2022-02-18 18:33:01 -0800

    Added Combat namespace and classes

--- Assets/Scripts/Control/PlayerController.cs
+++ Assets/Scripts/Control/PlayerController.cs
@@ -2,13 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+using RPG.Combat;
 using RPG.Movement;
+using System;
 
 namespace RPG.Control
 {
     public class PlayerController : MonoBehaviour
     {
         void Update()
+        {
+            InteractWithCombat();
+            InteractWithMovement();
+        }
+
+        void InteractWithCombat()
+        {
+            RaycastHit[] hits = Physics.RaycastAll(GetMouseRay());
+            foreach (RaycastHit hit in hits)
+            {
+                CombatTarget target = hit.transform.GetComponent<CombatTarget>();
+                if (target)
+                {
+                    if (Input.GetMouseButtonDown(0))
+                    {
+                        GetComponent<Fighter>().Attack(target);
+                    }
+                }
+            }
+        }
+
+        void InteractWithMovement()
         {
             if (Input.GetMouseButton(0))
             {
@@ -18,14 +42,18 @@ namespace RPG.Control
 
         void MoveToCursor()
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
-            bool hasHit = Physics.Raycast(ray, out hit);
+            bool hasHit = Physics.Raycast(GetMouseRay(), out hit);
 
             if (hasHit)
             {
                 GetComponent<Mover>().MoveTo(hit.point);
             }
+        }
+
+        static Ray GetMouseRay()
+        {
+            return Camera.main.ScreenPointToRay(Input.mousePosition);
         }
     }
 }
