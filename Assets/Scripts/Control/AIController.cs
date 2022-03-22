0b8cb5dae10825739abffcd5076dd40d54fd8571
Junwon <junwon.kaihatsu@gmail.com>
2022-03-22 09:24:31 -0700

    Set variables to null to avoid warnings

--- Assets/Scripts/Control/AIController.cs
+++ Assets/Scripts/Control/AIController.cs
@@ -11,7 +11,7 @@ namespace RPG.Control
     {
         [SerializeField] float chaseDistance = 5f;
         [SerializeField] float suspicionTime = 5f;
-        [SerializeField] PatrolPath patrolPath;
+        [SerializeField] PatrolPath patrolPath = null;
         [SerializeField] float waypointTolerance = 1f;
         [SerializeField] float waypointDwellTime = 5f;
         [Range(0, 1)]
